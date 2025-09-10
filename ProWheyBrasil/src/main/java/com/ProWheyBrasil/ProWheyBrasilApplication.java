8911ed23f6c8303d494ecbd53a3217c1914aac3a
Marcelo <marcelodossantosmachado05@gmail.com>
2025-09-10 16:16:31 -0300

    Feature Primeiro commit organizando pastas e workspace.

---
+++ ProWheyBrasil/src/main/java/com/ProWheyBrasil/ProWheyBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.ProWheyBrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProWheyBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProWheyBrasilApplication.class, args);
+	}
+
+}
---
+++ ProWheyBrasil/src/test/java/com/ProWheyBrasil/ProWheyBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProWheyBrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProWheyBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
