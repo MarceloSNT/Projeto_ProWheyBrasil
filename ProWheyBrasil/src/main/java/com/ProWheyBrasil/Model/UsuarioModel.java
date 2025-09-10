10611097f9f59f1858c1dc06d62572176a8ba6ea
Marcelo <marcelodossantosmachado05@gmail.com>
2025-09-10 16:16:10 -0300

    Feature Primeiro commit organizando pastas e workspace.

---
+++ ProWheyBrasil/src/main/java/com/ProWheyBrasil/Model/UsuarioModel.java
@@ -0,0 +1,24 @@
+package com.ProWheyBrasil.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TBUSUARIOS")
+public class UsuarioModel {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDUSUARIO")
+    private Integer idUsuario;
+    @Column(name = "NOMEUSUARIO")
+    private String nomeUsuario;
+    @Column(name = "EMAILUSUARIO")
+    @Email private String emailUsuario;
+    @Column(name = "SENHAUSUARIO")
+    private String senhaUsuario;
+}
